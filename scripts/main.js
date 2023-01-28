f0f85de3e60de85a18b3882ce4d7aee7038cb6f9
Acha0203 <74553433+Acha0203@users.noreply.github.com>
2023-01-29 04:23:35 +0900

    feat: create checkMove function

--- scripts/main.js
+++ scripts/main.js
@@ -20,12 +20,24 @@ let tetro = [
   [0, 0, 0, 0],
 ];
 
+//テトロミノの座標
 let tetro_x = 0;
 let tetro_y = 0;
 
 //フィールド本体
 let field = [];
 
+let can = document.getElementById('can');
+let con = can.getContext('2d');
+
+can.width = SCREEN_W;
+can.height = SCREEN_H;
+can.style.border = '4px solid #555';
+
+init();
+drawAll();
+
+//初期化
 function init() {
   //フィールドのクリア
   for (let y = 0; y < FIELD_ROW; y++) {
@@ -40,15 +52,15 @@ function init() {
   field[19][0] = 1;
 }
 
-let can = document.getElementById('can');
-let con = can.getContext('2d');
-
-can.width = SCREEN_W;
-can.height = SCREEN_H;
-can.style.border = '4px solid #555';
-
-init();
-drawAll();
+//ブロック１つを描画する
+function drawBlock(x, y) {
+  let px = x * BLOCK_SIZE;
+  let py = y * BLOCK_SIZE;
+  con.fillStyle = 'red';
+  con.fillRect(px, py, BLOCK_SIZE, BLOCK_SIZE);
+  con.strokeStyle = 'black';
+  con.strokeRect(px, py, BLOCK_SIZE, BLOCK_SIZE);
+}
 
 //全部描画する
 function drawAll() {
@@ -71,29 +83,43 @@ function drawAll() {
   }
 }
 
-//ブロック１つを描画する
-function drawBlock(x, y) {
-  let px = x * BLOCK_SIZE;
-  let py = y * BLOCK_SIZE;
-  con.fillStyle = 'red';
-  con.fillRect(px, py, BLOCK_SIZE, BLOCK_SIZE);
-  con.strokeStyle = 'black';
-  con.strokeRect(px, py, BLOCK_SIZE, BLOCK_SIZE);
+//ブロックの衝突判定
+function checkMove(mx, my) {
+  for (let y = 0; y < TETRO_SIZE; y++) {
+    for (let x = 0; x < TETRO_SIZE; x++) {
+      if (tetro[y][x]) {
+        let nx = tetro_x + mx + x;
+        let ny = tetro_y + my + y;
+
+        if (
+          ny < 0 ||
+          nx < 0 ||
+          ny >= FIELD_ROW ||
+          nx >= FIELD_COL ||
+          field[ny][nx]
+        ) {
+          return false;
+        }
+      }
+    }
+  }
+
+  return true;
 }
 
 document.onkeydown = function (e) {
   switch (e.key) {
     case 'ArrowLeft': // 左
-      tetro_x--;
+      if (checkMove(-1, 0)) tetro_x--;
       break;
     case 'ArrowUp': // 上
-      tetro_y--;
+      if (checkMove(0, -1)) tetro_y--;
       break;
     case 'ArrowRight': // 右
-      tetro_x++;
+      if (checkMove(1, 0)) tetro_x++;
       break;
     case 'ArrowDown': // 下
-      tetro_y++;
+      if (checkMove(0, 1)) tetro_y++;
       break;
     case ' ': // スペース
       break;
