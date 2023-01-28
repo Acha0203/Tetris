0bf37d37c9afd19cffeba2ea2edd3c4a74222362
Acha0203 <74553433+Acha0203@users.noreply.github.com>
2023-01-29 04:01:50 +0900

    feat: draw a field

--- scripts/main.js
+++ scripts/main.js
@@ -23,53 +23,81 @@ let tetro = [
 let tetro_x = 0;
 let tetro_y = 0;
 
+//フィールド本体
+let field = [];
+
+function init() {
+  //フィールドのクリア
+  for (let y = 0; y < FIELD_ROW; y++) {
+    field[y] = [];
+    for (let x = 0; x < FIELD_COL; x++) {
+      field[y][x] = 0;
+    }
+  }
+
+  field[5][8] = 1;
+  field[19][9] = 1;
+  field[19][0] = 1;
+}
+
 let can = document.getElementById('can');
 let con = can.getContext('2d');
 
 can.width = SCREEN_W;
 can.height = SCREEN_H;
 can.style.border = '4px solid #555';
-drawTetro();
 
-//テトロミノを描画する
-function drawTetro() {
+init();
+drawAll();
+
+//全部描画する
+function drawAll() {
   con.clearRect(0, 0, SCREEN_W, SCREEN_H);
+
+  for (let y = 0; y < FIELD_ROW; y++) {
+    for (let x = 0; x < FIELD_COL; x++) {
+      if (field[y][x]) {
+        drawBlock(x, y);
+      }
+    }
+  }
 
   for (let y = 0; y < TETRO_SIZE; y++) {
     for (let x = 0; x < TETRO_SIZE; x++) {
       if (tetro[y][x]) {
-        let px = (tetro_x + x) * BLOCK_SIZE;
-        let py = (tetro_y + y) * BLOCK_SIZE;
-        con.fillStyle = 'red';
-        con.fillRect(px, py, BLOCK_SIZE, BLOCK_SIZE);
-        con.strokeStyle = 'black';
-        con.strokeRect(px, py, BLOCK_SIZE, BLOCK_SIZE);
+        drawBlock(tetro_x + x, tetro_y + y);
       }
     }
   }
+}
+
+//ブロック１つを描画する
+function drawBlock(x, y) {
+  let px = x * BLOCK_SIZE;
+  let py = y * BLOCK_SIZE;
+  con.fillStyle = 'red';
+  con.fillRect(px, py, BLOCK_SIZE, BLOCK_SIZE);
+  con.strokeStyle = 'black';
+  con.strokeRect(px, py, BLOCK_SIZE, BLOCK_SIZE);
 }
 
 document.onkeydown = function (e) {
   switch (e.key) {
     case 'ArrowLeft': // 左
       tetro_x--;
-      console.log(tetro_x);
       break;
     case 'ArrowUp': // 上
       tetro_y--;
-      console.log(tetro_y);
       break;
     case 'ArrowRight': // 右
       tetro_x++;
-      console.log(tetro_x);
       break;
     case 'ArrowDown': // 下
       tetro_y++;
-      console.log(tetro_y);
       break;
     case ' ': // スペース
       break;
   }
 
-  drawTetro();
+  drawAll();
 };
